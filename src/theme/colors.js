674e1cf326cfe8ab0da70e589820eb2c11b34031
Mitch "The Blind" Horne <mitchhorne92@gmail.com>
2019-06-01 19:09:02 +0200

    Add submission form colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -4,6 +4,9 @@ export const colors = {
   danger: "#ff2323",
   focusAccent: "#b1bfcd",
   hoverAccent: "#ff9200",
+  inputBackground: "#f5f8f9",
+  inputBorder: "#cad7dd",
+  inpurtPlaceholder: "#ccc",
   inputText: "#373d40",
   positiveAccent: "#60e6c5",
   primaryText: "#383838"
