cacd568b47e07b70afe43e79017e75858d6b6552
Mitch "The Blind" Horne <mitchhorne92@gmail.com>
2019-06-01 09:56:01 +0200

    Add state and function to handle overlay activation

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
-// import propTypes from "prop-types";
 
 import { FlexContainer } from "../../components/flexContainer";
 import { SideNav } from "../../components/sideNav";
+import ProfileImageSrc from "../../assets/profile.png";
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { overlayActive: false };
+
+    this.toggleOverlay = this.toggleOverlay.bind(this);
+  }
+
+  toggleOverlay() {
+    this.setState({ ...this.state, overlayActive: !this.state.overlayActive });
+  }
+
   render() {
+    const { overlayActive } = this.state;
+
     return (
       <FlexContainer>
-        <SideNav />
+        <SideNav
+          overlayActive={overlayActive}
+          profileImage={ProfileImageSrc}
+          toggleOverloay={this.toggleOverlay}
+        />
       </FlexContainer>
     );
   }
 }
-
-// Home.propTypes = {};
--- src/containers/home/tests/index.test.js
+++ src/containers/home/tests/index.test.js
@@ -8,4 +8,18 @@ describe("Home", () => {
   it("should render without crashing", () => {
     shallow(<Home />);
   });
+
+  describe("toggleOverlay", () => {
+    it("should toggle state.overlayActive between true/false", () => {
+      const wrapper = shallow(<Home />);
+      const initialOverlayState = wrapper.state().overlayActive;
+
+      wrapper.instance().toggleOverlay();
+
+      const alteredOverlayState = wrapper.state().overlayActive;
+
+      expect(initialOverlayState).toEqual(false);
+      expect(alteredOverlayState).toEqual(true);
+    });
+  });
 });
