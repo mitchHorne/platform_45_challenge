1cbe8d36dced45ac1cf9d0b423b34287f1f8aa82
Mitch "The Blind" Horne <mitchhorne92@gmail.com>
2019-06-01 09:56:15 +0200

    Add overlay toggle button

--- src/components/sideNav/index.js
+++ src/components/sideNav/index.js
@@ -1,33 +1,63 @@
 import React from "react";
 import styled from "styled-components";
 
-import ProfileImageSrc from "../../assets/profile.png";
+import overlayToggleSrc from "../../assets/arrow.png";
+
+export const Nav = styled.div`
+  background: ${props => props.theme.colors.accent};
+  display: flex;
+  height: 90vh;
+  padding: 5vh 5%;
+  position: fixed;
+  width: 20%;
+`;
 
 export const Container = styled.div`
   align-items: center;
-  background: ${props => props.theme.colors.accent};
   display: flex;
+  flex: 1 1 auto;
   flex-direction: column;
-  height: 100vh;
-  justify-content: center;
-  position: fixed;
-  width: 25%;
-
-  p {
-    text-align: center;
-    width: 75%;
-  }
+  justify-content: space-around;
+  position: relative;
+  width: 100%;
 `;
 
 export const ProfileImage = styled.img`
-  padding: 0 0 7.5em;
-  width: 50%;
+  width: 60%;
 `;
 
-export const SideNav = () => (
-  <Container>
-    <ProfileImage src={ProfileImageSrc} />
-    <h1>Front-end challenge!</h1>
-    <p>This is a design that you will need to code up and impress us.</p>
-  </Container>
-);
+export const OverlayToggleButton = styled.img`
+  transition: transform 0.25s;
+
+  ${props => (props.overlayActive ? "transform: rotate(180deg);" : "")}
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const SideNav = props => {
+  const { overlayActive, profileImage, toggleOverloay } = props;
+  return (
+    <Nav>
+      <Container>
+        <ProfileImage src={profileImage} />
+        <div>
+          <h1>Front-end challenge!</h1>
+          <p>This is a design that you will need to code up and impress us.</p>
+          <p>
+            Hope my work impresses you{" "}
+            <span role="img" aria-label="Tongue out">
+              &#x1F61C;
+            </span>
+          </p>
+        </div>
+        <OverlayToggleButton
+          onClick={() => toggleOverloay()}
+          overlayActive={overlayActive}
+          src={overlayToggleSrc}
+        />
+      </Container>
+    </Nav>
+  );
+};
--- src/components/sideNav/tests/index.test.js
+++ src/components/sideNav/tests/index.test.js
@@ -4,7 +4,13 @@ import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 
 import { theme } from "../../../theme";
-import { Container, ProfileImage, SideNav } from "../";
+import {
+  Container,
+  Nav,
+  OverlayToggleButton,
+  ProfileImage,
+  SideNav
+} from "../";
 
 describe("SideNav", () => {
   it("should render without crashing", () => {
@@ -15,9 +21,40 @@ describe("SideNav", () => {
     it("should render without crashing", () => {
       shallow(<Container theme={theme} />);
     });
+  });
+
+  describe("OverlayToggleButton", () => {
+    it("should render without crashing", () => {
+      renderer.create(<OverlayToggleButton />);
+    });
+
+    it("should have a transform property of rotate(180deg) if prop overlayActive is true", () => {
+      const component = renderer
+        .create(<OverlayToggleButton overlayActive={true} />)
+        .toJSON();
+      expect(component).toHaveStyleRule("transform", "rotate(180deg)");
+    });
+
+    it("should call toggleOverloay on click", () => {
+      const toggleOverloayMock = jest.fn();
+      const wrapper = shallow(<SideNav toggleOverloay={toggleOverloayMock} />);
+
+      wrapper
+        .find(OverlayToggleButton)
+        .props()
+        .onClick();
+
+      expect(toggleOverloayMock).toHaveBeenCalled();
+    });
+  });
+
+  describe("Nav", () => {
+    it("should render without crashing", () => {
+      shallow(<Nav theme={theme} />);
+    });
 
     it("should have a background of theme.colors.accent", () => {
-      const component = renderer.create(<Container theme={theme} />).toJSON();
+      const component = renderer.create(<Nav theme={theme} />).toJSON();
       expect(component).toHaveStyleRule("background", theme.colors.accent);
     });
   });
