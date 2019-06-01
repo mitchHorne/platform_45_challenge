3e50c11c8668928cbece7208274f4b57f4ff667c
Mitch "The Blind" Horne <mitchhorne92@gmail.com>
2019-06-01 19:58:29 +0200

    Cover toggleValid function

--- src/containers/home/tests/index.test.js
+++ src/containers/home/tests/index.test.js
@@ -21,4 +21,18 @@ describe("Home", () => {
       expect(alteredVal).toEqual(true);
     });
   });
+
+  describe("toggleValid", () => {
+    it("should toggle state.valid between true/false", () => {
+      const wrapper = utils.generateEnzymeWrapper(<Home />);
+      const initialVal = utils.getStateProperty(wrapper, "valid");
+
+      utils.runComponentClassFunction(wrapper, "toggleValid");
+
+      const alteredVal = utils.getStateProperty(wrapper, "valid");
+
+      expect(initialVal).toEqual(false);
+      expect(alteredVal).toEqual(true);
+    });
+  });
 });
