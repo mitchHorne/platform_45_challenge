c081c4b3bc6590fb75ae7a32c441f82b27703cf8
Mitch "The Blind" Horne <mitchhorne92@gmail.com>
2019-06-02 13:39:45 +0200

    Add icon colors to theme

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -2,12 +2,12 @@ export const colors = {
   accent: "#ffd500",
   accentAlt: "#ff9200",
   danger: "#ff2323",
-  focusAccent: "#b1bfcd",
-  hoverAccent: "#ff9200",
+  accentLight: "#b1bfcd",
   inputBackground: "#f5f8f9",
   inputBorder: "#cad7dd",
   inpurtPlaceholder: "#ccc",
   inputText: "#373d40",
   positiveAccent: "#60e6c5",
-  primaryText: "#383838"
+  primaryText: "#383838",
+  radioUnselectedBackground: "#f5f8f9"
 };
